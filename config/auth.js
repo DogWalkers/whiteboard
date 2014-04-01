2fba6097862fbe32cc674f23b5936bbe71a3f9ad
Sahil Jain <sahil.jain@live.ca>
2014-03-31 23:46:43 -0400

    local running functionality

--- config/auth.js
+++ config/auth.js
@@ -1,10 +1,21 @@
-module.exports = {
-
-	'facebookAuth' : {
-		'clientID' 		: '215699211971159', // your App ID
-		'clientSecret' 	: '7ebadc6ebbccc3f816eba5c1054fbf34', // your App Secret
-		'callbackURL' 	: 'http://www.uwwhiteboard.com/auth/facebook/callback'
-	}//,
+	if(process.env.OPENSHIFT_NODEJS_IP === "undefined"){
+		module.exports = {
+			'facebookAuth' : {
+				'clientID' 		: '690942394285097', // your App ID
+				'clientSecret' 	: '00a33fd027f033dc1ae77ae67142123e', // your App Secret
+				'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+			}
+		};
+	}else{
+		module.exports = {
+			'facebookAuth' : {
+				'clientID' 		: '215699211971159', // your App ID
+				'clientSecret' 	: '7ebadc6ebbccc3f816eba5c1054fbf34', // your App Secret
+				'callbackURL' 	: 'http://www.uwwhiteboard.com/auth/facebook/callback'
+			}
+		};
+	}
+//,
 
 	/*'twitterAuth' : {
 		'consumerKey' 		: 'your-consumer-key-here',
@@ -17,5 +28,3 @@ module.exports = {
 		'clientSecret' 	: 'your-client-secret-here',
 		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
 	}*/
-
-};
